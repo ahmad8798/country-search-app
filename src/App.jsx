fabb8b8cbd17ba2013258f4659c3df40adfad77d
ahmad <shaikahmadpasha8798@gmail.com>
2024-08-22 21:43:42 +0530

    create a home page before i am just connecting the components in app.jsx but now created separte page to display home page. from now i am going to create pages by connecting the required components to make that page

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from "react";
-import CountryCard from "./components/Card/CountryCard";
-
-import Header from "./components/Header/Header";
-import CardList from "./components/CardList/CardList";
-import RegionsDropDownButton from "./components/RegionsDropDown/RegionsDropDownButton";
-import SearchBar from "./components/SearchBar/SearchBar";
-import SearchFilterBar from "./components/SearchFilterBar/SearchFilterBar";
-import useFetch from "./hooks/useFetch";
+import Home from "./pages/Home";
 
 const App = () => {
-  const { data, loading, error } = useFetch("https://restcountries.com/v3.1/all");
-  
-  const [countries, setCountries] = useState([]);
-  
-
-  // Update the countries state when data is fetched
-  useEffect(() => {
-    if (data) {
-      setCountries(data);
-    }
-  }, [data]);
 
   return (
     <>
-      <Header />
-      <SearchFilterBar className={"mt-5"}>
-        <SearchBar />
-        <RegionsDropDownButton />
-      </SearchFilterBar>
-
-      {error && <p className="text-danger">Error: {error}</p>}
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <CardList>
-          {countries.map((country) => (
-            <CountryCard key={country.cca3} country={country} />
-          ))}
-        </CardList>
-      )}
+      <Home/>
     </>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import CountryCard from "../components/Card/CountryCard"
+import CardList from "../components/CardList/CardList"
+import Header from "../components/Header/Header"
+import RegionsDropDownButton from "../components/RegionsDropDown/RegionsDropDownButton"
+import SearchBar from "../components/SearchBar/SearchBar"
+import SearchFilterBar from "../components/SearchFilterBar/SearchFilterBar"
+import useFetch from "../hooks/useFetch"
+
+
+const Home = () => {
+    const { data, loading, error } = useFetch("https://restcountries.com/v3.1/all");
+  
+    const [countries, setCountries] = useState([]);
+    
+  
+    // Update the countries state when data is fetched
+    useEffect(() => {
+      if (data) {
+        setCountries(data);
+      }
+    }, [data]);
+  return (
+    <>
+    <Header />
+    <SearchFilterBar className={"mt-5"}>
+      <SearchBar />
+      <RegionsDropDownButton />
+    </SearchFilterBar>
+
+    {error && <p className="text-danger">Error: {error}</p>}
+    {loading ? (
+      <p>Loading...</p>
+    ) : (
+      <CardList>
+        {countries.map((country) => (
+          <CountryCard key={country.cca3} country={country} />
+        ))}
+      </CardList>
+    )}
+  </>
+  )
+}
+
+export default Home
